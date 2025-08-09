3fd1d977917476d7ed263bd42e9307ed7c0d2e11
venky1112r <venky1112r@gmail.com>
2025-08-09 17:22:05 +0530

    build to host github

--- My Portfolio/my-portfolio/vite.config.js
+++ My Portfolio/my-portfolio/vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Projects/My Portfolio/', // 👈 Replace with your repo name
   plugins: [
     react(),
-     tailwindcss()
+    tailwindcss()
   ],
 })
